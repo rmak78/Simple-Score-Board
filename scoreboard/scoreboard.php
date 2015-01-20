9cc8c0220576866206f0272158f73d8b58e624b5
Rana Mansoor Akbar Khan <mansoor@sutlej.net>
2015-01-21 00:40:10 +0500

    Added some functions
    some functions added for waseem to work on

--- scoreboard/scoreboard.php
+++ scoreboard/scoreboard.php
@@ -26,7 +26,44 @@ Copyright 2015  Sutlej.net
  // Copy Paste Plugin Function from Excercise File Chapter3: KAMRAN
  
  // Copy paste Templates for admin page From Excersise File Chapter5: MANSOOR
- 
+ function score_borad_init()
+{
+	register_setting('score_board_settings','score_board_team1_name');
+	register_setting('score_board_settings','score_board_team2_name');
+	register_setting('score_board_settings','score_board_team3_name');
+	register_setting('score_board_settings','score_board_team4_name');
+	register_setting('score_board_settings','score_board_team1_score');
+	register_setting('score_board_settings','score_board_team2_score');
+	register_setting('score_board_settings','score_board_team3_score');
+	register_setting('score_board_settings','score_board_team4_score');
+}
+add_action('admin_init','score_borad_init');
+
+function score_board_option_page()
+{
+	?>
+	<div class="wrap"><?php screen_icon(); ?>
+	<h2>Score Board Option Page</h2>
+	<p> Type explanation text here</p>
+	<form action="options.php" method="post" id="score-board-options-form">
+	<?php settings_fields('score_board_settings'); ?>
+	<h3><label for="score_board_team1_name">Team 1 Name: </label> <input
+		type="text" id="score_board_team1_name" name="score_board_team1_name"
+		value="<?php echo esc_attr( get_option('score_board_team1_name') ); ?>" /></h3>
+	<p><input type="submit" name="submit" value="Update Score Board" /></p>
+	</form>
+	</div>
+	<?php
+}
+/* FIX THIS : WASEEM
+function cccomm_plugin_menu()
+{
+	
+	add_settings_page('cc_comments-plugin','CC Comments Options 2', 'CC Comments Options 2', 'manage_options', 'cc_comments-plugin2', 'score_board_option_page');
+}
+add_action('admin_menu', 'cccomm_plugin_menu');
+
+*/
   // Create and add Admin Page: WASEEM
  
  
