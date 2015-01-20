a7c43186b4351d20bf91abdd61888c12fd24682d
Rana Mansoor Akbar Khan <mansoor@sutlej.net>
2015-01-21 00:12:36 +0500

    Added Liscence Information
    Jo bhi tha

--- scoreboard/scoreboard.php
+++ scoreboard/scoreboard.php
@@ -6,5 +6,19 @@ Description: This Plugin will display scores of four teams on the webpage
 Author: Rana Mansoor Akbar Khan, Muhammad Wasim, Kamran Sarwar
 Version: 1.0
 Author URI: http://www.sutlej.net/
+
+Copyright 2015  Sutlej.net
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA 
  */
- //Testing This File
