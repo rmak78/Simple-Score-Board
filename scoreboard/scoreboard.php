83dec01e5624566ff4a45e238888bde920226296
waseem-mehmood <waseem@sutlej.net>
2015-01-21 00:01:44 +0500

    Update scoreboard.php
    Edit name

--- scoreboard/scoreboard.php
+++ scoreboard/scoreboard.php
@@ -3,7 +3,7 @@
 Plugin Name: Score Board Plugin
 Plugin URI: http://www.sutlej.net/plugins/
 Description: This Plugin will display scores of four teams on the webpage
-Author: Rana Mansoor Akbar Khan, Muhammad Waseem, Kamran Sarwar
+Author: Rana Mansoor Akbar Khan, Muhammad Wasim, Kamran Sarwar
 Version: 1.0
 Author URI: http://www.sutlej.net/
  */
