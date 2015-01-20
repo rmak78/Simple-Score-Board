e1d195806278622ac2f9ceb4badceed8e9164fda
Rana Mansoor Akbar Khan <rmak@sutlej.net>
2015-01-20 23:21:36 +0500

    Update scoreboard.php
    Added Template

---
+++ scoreboard/scoreboard.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Score Board Plugin
+Plugin URI: http://www.sutlej.net/plugins/
+Description: This plugin will CC us when a post is commented
+Author: Rana Mansoor Akbar Khan, Muhammad Waseem, Kamran Sarwar
+Version: 1.0
+Author URI: http://www.sutlej.net/
+ */
