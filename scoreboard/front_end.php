05d4a98a13d3bcc2c831bf20c498168f5fe06026
Kamran-Sarwar <kamran.sarwar@sutlej.net>
2015-01-22 00:17:17 +0500

    Update front_end.php

--- scoreboard/front_end.php
+++ scoreboard/front_end.php
@@ -2,116 +2,218 @@
 // Frontend code comes here
 function show_score_board() {
 	$score_board = "
-	<style type='text/css'>
-#score-board-container {
-	width: 350px;
-	height: 75px;
-	position: fixed;
+body {
+	font-family: Comic Sans MS, cursive, sans-serif;
+	color: white;
+}
+.container {
+	width: 390px;
+	height: 350px;
+	position: absolute;
 	bottom: 0px;
-	right: 49px;
-	z-index: 100;
+	right: 50px;
+	transition: all .5s linear;
+	-webkit-transition: all .5s linear;
+	-moz-transition: all .5s linear;
+	-o-transition: all .5s linear;
+	overflow: hidden;
+	height: 85px;
 }
-
-#score-board-container:hover {
-	height: 200px;
+.container:hover {
+	height: 290px;
 }
-
-#banner {
-	border: 1px solid;
-	margin: 0px auto;
-	width: 70%;
-	background-color: white;
-	z-index: 1;
+.banner {
+	width: 250px;
+	height: 40px;
+	z-index: 5;
+	line-height: 200px;
+	background: #f9f9f9;
+	background: -moz-linear-gradient(top,  rgba(248,248,248,1) 0%, rgba(249,249,249,1) 100%);
+	background: -webkit-linear-gradient(top,  rgba(248,248,248,1) 0%,rgba(249,249,249,1) 100%);
+	background: -o-linear-gradient(top,  rgba(248,248,248,1) 0%,rgba(249,249,249,1) 100%);
+	background: -ms-linear-gradient(top,  rgba(248,248,248,1) 0%,rgba(249,249,249,1) 100%);
+	background: linear-gradient(top,  rgba(248,248,248,1) 0%,rgba(249,249,249,1) 100%);
+	-webkit-box-shadow: 0 1px 0 #fff inset;
+	-moz-box-shadow: 0 1px 0 #fff inset;
+	-ms-box-shadow: 0 1px 0 #fff inset;
+	-o-box-shadow: 0 1px 0 #fff inset;
+	box-shadow: 0 1px 0 #fff inset;
+	border: 1px solid #c4c6ca;
+	margin: 5px auto;
+	padding: 25px 0 0;
 	position: relative;
-	margin-bottom: -10px;
 	text-align: center;
+	text-shadow: 0 1px 0 #fff;
 }
 
-#team-board {
-	background-color:#900;
-	height:inherit;
-}
-
-#team-1 {
-	background-color: blue;
-}
-
-#team-2 {
-	background-color:orange;
-}
-
-#team-3 {
-	background-color:#6EE670;
-}
-
-#team-4 {
-	background-color:red;
-}
-
-.team{
+.banner span {
 	display: inline-block;
-	height: inherit;
-	width: 62px;
-	margin-top: 29px;
-	margin-left: 15px;
-	color: #FFF;
-	}
-	
+	vertical-align: middle;
+	line-height: normal;
+	font-size: 16px;
+	font-weight: bold;
+}
+
+.banner:after,
+.banner:before {
+	background: #f9f9f9;
+	background: -moz-linear-gradient(top,  rgba(248,248,248,1) 0%, rgba(249,249,249,1) 100%);
+	background: -webkit-linear-gradient(top,  rgba(248,248,248,1) 0%,rgba(249,249,249,1) 100%);
+	background: -o-linear-gradient(top,  rgba(248,248,248,1) 0%,rgba(249,249,249,1) 100%);
+	background: -ms-linear-gradient(top,  rgba(248,248,248,1) 0%,rgba(249,249,249,1) 100%);
+	background: linear-gradient(top,  rgba(248,248,248,1) 0%,rgba(249,249,249,1) 100%);
+	border: 1px solid #c4c6ca;
+	content: "";
+	display: block;
+	height: 100%;
+	left: -1px;
+	position: absolute;
+	width: 100%;
+}
+.banner:after {
+	-webkit-transform: rotate(2deg);
+	-moz-transform: rotate(2deg);
+	-ms-transform: rotate(2deg);
+	-o-transform: rotate(2deg);
+	transform: rotate(2deg);
+	top: 0;
+	z-index: -1;
+	-webkit-box-shadow: 0 8px 6px -6px black;
+	-moz-box-shadow: 0 8px 6px -6px black;
+	box-shadow: 0 8px 6px -6px black;
+}
+.banner:before {
+	-webkit-transform: rotate(-3deg);
+	-moz-transform: rotate(-3deg);
+	-ms-transform: rotate(-3deg);
+	-o-transform: rotate(-3deg);
+	transform: rotate(-3deg);
+	top: 0;
+	z-index: -2;
+	-webkit-box-shadow: 0 -8px 6px -6px black;
+	-moz-box-shadow: 0 -8px 6px -6px black;
+	box-shadow: 0 -8px 6px -6px black;
+}
+
+.board-container {
+	width: 370px;
+	height: 240px;
+	background: #5C1F00;
+	position: relative;
+	top: -35px;
+	z-index: 2;
+	padding: 5px;
+	margin: 5px auto;
+}
+.board-1 {
+	width: 80px;
+	height: 200px;
+	background-color: #1975FF;
+	display: inline-block;
+	float: left;
+	margin-left: 10px;
+	margin-top: 50px;
+}
+
+.board-2 {
+	width: 80px;
+	height: 200px;
+	background-color: #FF9900;
+	display: inline-block;
+	float: left;
+	margin-left: 10px;
+	margin-top: 50px;
+}
+
+.board-3 {
+	width: 80px;
+	height: 200px;
+	background-color: #92E228;
+	display: inline-block;
+	float: left;
+	margin-left: 10px;
+	margin-top: 50px;
+}
+
+.board-4 {
+	width: 80px;
+	height: 200px;
+	background-color: #FF0000;
+	display: inline-block;
+	float: left;
+	margin-left: 10px;
+	margin-top: 50px;
+	margin-right: 10px;
+}
+
 #team-name{	
 	transform: rotate(90deg);
-	overflow: auto;
-	height: 80px;
+	height: 50px;
 	text-align: center;
-	}
+	vertical-align: middle;
+	width: 100px;
+	margin-top: 50%;
+	margin-left: -14px;
+}
 	
 #team-score{
-	bottom:0;
+	font-size: 22px;
+	margin-top: 50px;
     text-align: center;
-    padding: 18px;
-	}
-	
+}	
 </style>
 
-<div id='score-board-container'>
-	<div id='content'>
-    <p> House point scores board</p>
-     </div>
-    <div id='team-board'>
-    	<div class='team' id='team-1'>
-        	<div id='team-name'>
-       	 		<p>".esc_attr( get_option('score_board_team1_name') )."</p>
-            </div>
-            <div id='team-score'>
-       	 		<p><b>".esc_attr( get_option('score_board_team1_score') )."</b></p>
-            </div>
-    	</div>
-        <div class='team' id='team-2'>
-        	<div id='team-name'>
-       	 		<p>".esc_attr( get_option('score_board_team2_name') )."</p>
-            </div>
-            <div id='team-score'>
-       	 		<p><b>".esc_attr( get_option('score_board_team2_score') )."</b></p>
-            </div>
-    	</div>
-        <div class='team' id='team-3'>
-        	<div id='team-name'>
-       	 		<p>".esc_attr( get_option('score_board_team3_name') )."</p>
-            </div>
-            <div id='team-score'>
-       	 		<p><b>".esc_attr( get_option('score_board_team3_score') )."</b></p>
-            </div>
-    	</div>
-        <div class='team' id='team-4'>
-        	<div id='team-name'>
-       	 		<p>".esc_attr( get_option('score_board_team4_name') )."</p>
-            </div>
-            <div id='team-score'>
-       	 		<p><b>."esc_attr( get_option('score_board_team4_score') )."</b></p>
-            </div>
-         </div>
+<div class='container'>
 
-    </div>
-    
+<div class='banner'>
+<span>
+<span style='color: #4573D4;'>House&nbsp;</span>
+<span style='color: #FF9900;'>Points&nbsp;</span>
+<span style='color: #92E228;'>Score&nbsp;</span>
+<span style='color: #FF0000;'>Board</span>
+</span>
+</div>
+
+<div class='board-container'>
+
+<div class='board-1'>
+	<div id='team-name'>
+	<p>".esc_attr( get_option('score_board_team1_name') )."</p>
+	</div>
+	<div id='team-score'>
+	<p><b>".esc_attr( get_option('score_board_team1_score') )."</b></p>
+	</div>
+</div>
+
+<div class='board-2'>
+	<div id='team-name'>
+	<p>".esc_attr( get_option('score_board_team2_name') )."</p>
+	</div>
+	<div id='team-score'>
+	<p><b>".esc_attr( get_option('score_board_team2_score') )."</b></p>
+	</div>
+</div>
+
+<div class='board-3'>
+	<div id='team-name'>
+	<p>".esc_attr( get_option('score_board_team3_name') )."</p>
+	</div>
+	<div id='team-score'>
+	<p><b>".esc_attr( get_option('score_board_team3_score') )."</b></p>
+	</div>
+</div>
+
+<div class='board-4'>
+	<div id='team-name'>
+	<p>".esc_attr( get_option('score_board_team4_name') )."</p>
+	</div>
+	<div id='team-score'>
+	<p><b>".esc_attr( get_option('score_board_team4_score') )."</b></p>
+	</div>
+</div>
+
+</div>
+
 </div>
 	";
 	echo $score_board;
